57dbefa7b5e53725de02cfaa7b127e9dee34dfad
Annabelle Mangold <amangold@1-B-27.42heilbronn.de>
2022-12-15 20:38:34 +0100

    commit

--- ft_split.c
+++ ft_split.c
@@ -6,137 +6,87 @@
 /*   By:  amangold < amangold@student.42heilbron    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/21 15:59:56 by  amangold         #+#    #+#             */
-/*   Updated: 2022/12/13 14:37:51 by  amangold        ###   ########.fr       */
+/*   Updated: 2022/12/15 20:35:03 by  amangold        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdlib.h>
-
 #include <stdio.h>
 
-static int	ft_counter(char const *s, char c)
+static int	ft_wrdcnt(char const *s, char c)
 {
 	size_t	i;
-	int		counter;
+	int		num;
 
 	i = 0;
-	counter = 0;
+	num = 0;
 	while (s[i] != '\0')
 	{
-		if (s[i] != c)
-			counter++;
+		while ((s[i] == c))
+			i++;
+		if (s[i] != c && s[i] != '\0')
+			num++;
 		while (s[i] != c && s[i] != '\0')
 			i++;
-		if (s[i] == c)
-			i++;
 	}
-	return (counter);
+	return (num);
 }
 
-static int	ft_word_len(char const *s, char c, int wn)
+static char	**free_split(char **res)
 {
-	int	length;
 	int	i;
-	int	counter;
 
 	i = 0;
-	counter = 0;
-	while (s[i] != '\0' && counter < wn)
+	while (res[i] != NULL)
 	{
-		if (s[i] != c)
-			counter++;
-		while (s[i] != c && s[i] != '\0')
-			i++;
-		if (s[i] == c)
-			i++;
-	}
-	while (s[i] == c)
+		free(res[i]);
 		i++;
-	// printf("%c\n", s[i]);
-	length = i;
-	while (s[length] != '\0' && s[length] != c)
-	{
-		length++;
 	}
-	return (length - i);
+	free(res);
+	return (NULL);
 }
 
-static char	**ft_word_writer(char **str, char const *s, char c)
+static char	**ft_split_words(char	**words, char const *s, char c)
 {
-	int	i;
-	int	j;
-	int	k;
+	size_t	i;
+	size_t	k;
+	int		num;
 
 	i = 0;
-	j = 0;
+	k = 0;
+	num = 0;
 	while (s[i] != '\0')
 	{
-		if (s[i] != c)
-		{
-			k = 0;
-			while (s[i] != c && s[i] != '\0')
-			{
-				str[j][k] = s[i];
-				i++;
-				k++;
-			}
-		// printf("word: %s\n", str[j]);
-			str[j][k] = '\0';
-			j++;
-		}
-		if (s[i] == c)
+		while ((s[i] == c))
 			i++;
+		while (s[i] != c && s[i] != '\0')
+		{
+			i++;
+			k++;
+		}
+		if (k > 0)
+		{
+			words[num] = ft_substr(s, i - k, k);
+			if (!words[num++])
+				return (free_split(words));
+			k = 0;
+		}
 	}
-	return (str);
-}
-
-static void	free_all(char **str, int i)
-{
-	while (i >= 0)
-	{
-		free(str[i]);
-		i--;
-	}
-	free(str);
+	return (words);
 }
 
 char	**ft_split(char const *s, char c)
 {
-	char	**str;
-	int		w_len;
+	char	**words;
 	int		i;
 
+	i = 0;
 	if (!s)
 		return (NULL);
-	str = (char **) malloc(sizeof(char *) * (ft_counter(s, c) + 1));
-	if (!str)
+	words = ft_calloc(ft_wrdcnt(s, c) + 1, sizeof(char *));
+	if (!words)
 		return (NULL);
-	i = -1;
-	while (++i < ft_counter(s, c))
-	{
-		w_len = ft_word_len(s, c, i);
-		// printf("allocating memory for length: %d\n", w_len);
-		str[i] = (char *)malloc((w_len + 1) * sizeof(char));
-		if (!str[i])
-		{
-			free_all(str, i);
-			return (NULL);
-		}
-	}
-	str = ft_word_writer(str, s, c);
-	// str[ft_counter(s,c) + 1] = 0;
-	return (str);
+	words = ft_split_words(words, s, c);
+	return (words);
 }
-
-
-// int main(void){
-// 	char *s = "lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse";
-// 	char **ret = ft_split(s, ' ');
-// 	int i = 0;
-// 	while (ret[i] != NULL)
-// 	{
-// 		printf("Word number %d: %s \n",i , ret[i]);
-// 		i++;
-// 	}
-// }
--- main.c
+++ main.c
@@ -6,14 +6,20 @@
 /*   By:  amangold < amangold@student.42heilbron    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/06 18:53:10 by  amangold         #+#    #+#             */
-/*   Updated: 2022/12/09 11:26:24 by  amangold        ###   ########.fr       */
+/*   Updated: 2022/12/15 19:40:59 by  amangold        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdio.h>
 
-int	main(void)
-{
-	printf("%i", ft_intlen(12356));
+int main(void){
+	char *s = "hello!";
+	char **ret = ft_split(s,' ');
+	int i = 0;
+	while (ret[i] != NULL)
+	{
+		printf("Word number %d: %s \n",i , ret[i]);
+		i++;
+	}
 }
